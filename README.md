9de57649aa086ec0ce3069763771696aa7cd3b50
OKOJ <aajmd@hotmail.com>
2019-04-29 10:37:52 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# omazon
+# Omazon
+
+It is an Amozon-like storefront with MySQL database backend.
+The Omazon will take in orders from customers and deplete stock from the store's inventory.
+
+### Set up
+
+1. Clone the repo
+
+2. `npm install`
+
+3. Run  `node omazon.js`
+
+4. Thats it! Bam!
+
+### App Demo
+#### for Costomers
+
+### Technologies Used:
+- Javascript
+- NodeJS
+- MySQL
+- npm packages:
+        * mysql
+        * prompt
--- omazonCustomer.js
+++ omazonCustomer.js
@@ -36,6 +36,7 @@ function start() {
 function choosingProduct() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+        //choose an item by item_id
         inquirer.prompt([
             {
                 type: "input",
@@ -47,6 +48,7 @@ function choosingProduct() {
             for (var i = 0; i < res.length; i++) {
                 if (product.items_id == res[i].item_id) {
                     var prod = i;
+        //choose a quantity
                     inquirer.prompt([{
                         type: "input",
                         name: "quantity",
@@ -55,23 +57,29 @@ function choosingProduct() {
                     }]).then(function (cart) {
                         if (res[prod].stock_quantity === 0) {
                             console.log("This item is out of stock")
+                            console.log("Would you like to choose an other item?");
+                            choosingProduct();
+
                         }
                         if (res[prod].stock_quantity >= cart.quantity) {
                             var total = (cart.quantity * res[prod].price).toFixed(2);
-                            console.log("You total is $" + total);
+                            console.log("-----------------------------------------")
+                            console.log("Thank you for your purchase.")
+                            console.log("Your order is placed. Total:  $" + total);
+                            console.log("-----------------------------------------")
                         }
                         if (res[prod].stock_quantity < cart.quantity) {
                             console.log("We have " + res[prod].stock_quantity + " in stock");
                         }
                         if ((res[prod].stock_quantity - cart.quantity) > 0) {
-                            connection.query("UPDATE Products SET stock_quantity = " + (res[prod].stock_quantity - cart.quantity) + 
-                            " WHERE item_id = " + res[prod].item_id, function (err, res2) {
-                                if (err) throw err;
-                                console.table(res2);
-                                connection.end()
-                    
-                            });
-                    
+                            connection.query("UPDATE Products SET stock_quantity = " + (res[prod].stock_quantity - cart.quantity) +
+                                " WHERE item_id = " + res[prod].item_id, function (err, res2) {
+                                    if (err) throw err;
+                                    //console.table(res2);
+                                    connection.end()
+
+                                });
+
                         }
 
 
@@ -82,22 +90,33 @@ function choosingProduct() {
         })
 
     });
+    //reStart()
+
 }
 
-/*function updateProduct() {
-    if ((res[prod].stock_quantity - cart.quantity) > 0) {
-        connection.query("UPDATE Products SET stock_quantity = " + (res[prod].stock_quantity - cart.quantity) + 
-        " WHERE item_id = " + res[prod].item_id, function (err, res2) {
-            if (err) throw err;
-            console.table(res2);
-            connection.end()
+/*function reStart() {
+    connection.query("SELECT * FROM products", function (err, res2) {
+        if (err) throw err;
+       // console.table(res2);
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Would you like to make an other purchase?",
+                choices: ["yes", "no"]
+            }
+        ]).then(function (answer) {
+            if (answer.choices === "yes") {
+                console.log(answer.choices);
+                //choosingProduct();
+            }
+            if (answer.choices === "no") {
+                connection.end();
+            }
 
-        });
-
-    }
-
-}*/
-
+        })
+    })
+}
+reStart()*/
 /*function purchase(){
     var total = cart.quantity * res[prod].price;
     console.log("You total is $" + total);
